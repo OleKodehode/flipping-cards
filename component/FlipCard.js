d69d8839b340061589a1c2b7bf1fe073053cdbd7
Ole Håvard Furuseth Bergan <oleh.kodehode@gmail.com>
2023-04-28 12:25:54 +0200

    Flip cards

---
+++ component/FlipCard.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+const FlipCard = ({ frontContent, backContent }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleFlip = () => {
+    setIsFlipped(!isFlipped);
+  };
+
+  return (
+    <div className={`flip-card ${isFlipped ? 'flipped' : ''}`} onClick={handleFlip}>
+      <div className="flip-card-inner">
+        <div className="flip-card-front">{frontContent}</div>
+        <div className="flip-card-back">{backContent}</div>
+      </div>
+    </div>
+  );
+};
+
+export default FlipCard;
